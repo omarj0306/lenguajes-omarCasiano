de66a9a69c0ee547e7a770f0892ec18a281bcfd1
omarj0306 <joseocm_yr@hotmail.com>
2014-09-15 19:42:38 -0500

    Update ProyectoOmar.java

--- src/proyecto/omar/ProyectoOmar.java
+++ src/proyecto/omar/ProyectoOmar.java
@@ -16,7 +16,7 @@ public class ProyectoOmar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("mi primer programa");
+       System.out.println("mi primer programa-omar");
     }
     
 }
